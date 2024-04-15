716a202f947bd9881d2c13f46314f0aefb4923ba
Ben Kirk <benkirk@ucar.edu>
2024-04-15 11:53:51 -0600

    test default PFL too

--- derecho/mpiio_lustre_perf/test_stripes.sh
+++ derecho/mpiio_lustre_perf/test_stripes.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-
-
+test="pio_noncontig"
 topdir=$(pwd)
+
+. config_env.sh || exit 1
+make ${test} || exit 1
 
 mkdir -p logs/
 
-LASTID="none"
+#--------------------------------------------
+# default PFL case
+casedir="${topdir}/logs/default_PFL/"
+echo "submitting ${casedir}"
+mkdir -p ${casedir} && cd ${casedir} || exit 1
+
+ln -s ${topdir}/config_env.sh .
+ln -s ${topdir}/${test} .
+
+LASTID=$(qsub -A SCSG0001 -N ${test}-default_PFL ${topdir}/run.pbs)
+
+#--------------------------------------------
+# sweep over stripes
 for size in 128M 064M 032M 016M 004M 001M; do
     for count in 02 04 06 12 24 48 96; do
         casedir="${topdir}/logs/c$(printf "%02d" ${count})_S${size}/"
@@ -15,12 +29,9 @@ for size in 128M 064M 032M 016M 004M 001M; do
         stripe_cmd="lfs setstripe -c ${count} -S ${size} ."
         eval "${stripe_cmd}"
 
-        ln -sf ${topdir}/Makefile .
-        ln -sf ${topdir}/config_env.sh .
+        ln -s ${topdir}/config_env.sh .
+        ln -s ${topdir}/${test} .
 
-        [[ ${LASTID} == "none" ]] \
-            && LASTID=$(qsub -A SCSG0001 -N pioperf-c${count}_S${size} ${topdir}/run.pbs) \
-                || LASTID=$(qsub -A SCSG0001 -N pioperf-c${count}_S${size} -W depend=afterany:${LASTID} ${topdir}/run.pbs)
-
+        LASTID=$(qsub -A SCSG0001 -N ${test}-c${count}_S${size} -W depend=afterany:${LASTID} ${topdir}/run.pbs)
     done
 done
